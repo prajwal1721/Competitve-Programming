120266fff85e3f5d05780dc067dee6e8e24f1503
PKS <pkssfs1721@gmail.com>
2019-12-12 14:54:48 +0530

    12_DEC

--- CF/55A.cpp
+++ CF/55A.cpp
@@ -1,11 +1,30 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 int main()
 {
     typedef long long ll;
     ll n;
     cin>>n;
+    ll a[n];
+    for(ll i=0;i<n;i++)
+        a[i]=0;
     if(n==1)cout<<"YES\n";
-    else if(n%2==0)cout<<"YES\n";
-    else cout<<"NO\n";
+    else 
+    {
+        ll p=0,count=0;
+
+        for(ll i=0;a[i]<=0;)
+        {
+            if(!a[i]){
+                a[i]++;
+                count++;
+            }
+            i=i+++p;
+            if(i>=n) i=i%n;
+            // cout<<i<<' ';
+        }   
+        if(count==n) cout<<"YES\n";
+        else cout<<"NO\n";
+    }
 }
